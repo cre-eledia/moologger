b65eeeebb8012338b96153480f8782970632f3ea
Christopher Reimann <cre@eledia.de>
2024-05-25 08:43:01 +0200

    renaming and adding of static methods for ease of access

--- moologger.php
+++ moologger.php
@@ -8,11 +8,11 @@ use Katzgrau\KLogger\Logger;
 use Psr\Log\LogLevel;
 
 set_exception_handler(function($exception) {
-    Moologger::getlogger()->error('Uncaught Exception: '.$exception->getMessage());
+    moologger::get_logger()->error('Uncaught Exception: '.$exception->getMessage());
     echo 'An error occurred. Please contact the administrator.';
 });
 
-class Moologger {
+class moologger {
     private static $logger = [];
 
     private function __construct($name) {
@@ -22,7 +22,7 @@ class Moologger {
         } else {
             $loglevel = LogLevel::ERROR;
         }
-        self::$logger[$name] = new Logger($CFG->dataroot . '/moodlelogs', $loglevel, [
+        self::$logger[$name] = new Logger($CFG->dataroot . '/plugin_logs', $loglevel, [
           'extension' => 'log',
           'prefix' => $name.'__',
             'flushFrequency' => 1,
@@ -35,14 +35,51 @@ class Moologger {
         return $plugin->component;
     }
 
-    public static function getlogger($name = null) {
+    public static function get_logger($name = null) {
         if (is_null($name)){
             $name = self::get_plugin_name();
         }
         if (!isset(self::$logger[$name])){
-            new Moologger($name);
+            new moologger($name);
         }
         return self::$logger[$name];
     }
+
+    public static function debug($message, $name = null) {
+        self::get_logger($name)->debug($message);
+    }
+
+    public static function info($message, $name = null) {
+        self::get_logger($name)->info($message);
+    }
+
+    public static function notice($message, $name = null) {
+        self::get_logger($name)->notice($message);
+    }
+
+    public static function warning($message, $name = null) {
+        self::get_logger($name)->warning($message);
+    }
+
+    public static function error($message, $name = null) {
+        self::get_logger($name)->error($message);
+    }
+
+    public static function critical($message, $name = null) {
+        self::get_logger($name)->critical($message);
+    }
+
+    public static function alert($message, $name = null) {
+        self::get_logger($name)->alert($message);
+    }
+
+    public static function emergency($message, $name = null) {
+        self::get_logger($name)->emergency($message);
+    }
+
+    public static function log($level, $message, $name = null) {
+        self::get_logger($name)->log($level, $message);
+    }
+
 }
 
